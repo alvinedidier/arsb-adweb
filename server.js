46efae76e0e753b4766294fa2ee9fae399b7c79c
Alvine DIDIER <alvinedidier@gmail.com>
2025-02-25 21:47:21 +0400

    MAJ données

--- server.js
+++ server.js
@@ -3,32 +3,35 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
+const hostname = process.env.NODE_ENV === 'production' ? 'localhost' : 'https://dev.antennesb.fr/'
 const port = process.env.PORT || 3000
-
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+    createServer(async (req, res) => {
+        try {
+            const parsedUrl = parse(req.url, true)
+            const { pathname, query } = parsedUrl
 
-      if (pathname === '/a') {
-        await app.render(req, res, '/a', query)
-      } else if (pathname === '/b') {
-        await app.render(req, res, '/b', query)
-      } else {
-        await handle(req, res, parsedUrl)
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  }).listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://${hostname}:${port}`)
-  })
+            if (pathname === '/a') {
+                await app.render(req, res, '/a', query)
+            } else if (pathname === '/b') {
+                await app.render(req, res, '/b', query)
+            } else {
+                await handle(req, res, parsedUrl)
+            }
+        } catch (err) {
+            console.error('Error occurred handling', err)
+            res.statusCode = 500
+            res.end('internal server error')
+        }
+    })
+    .on('error', (err) => {
+        console.error(err)
+        process.exit(1)
+    })
+    .listen(port, () => {
+        console.log(`> Ready on http://${hostname}:${port}`)
+    })
 })
